9c029751fb5ad773c363195a1ead8fe62729143f
Sumat Mallick <115460605+SumatM@users.noreply.github.com>
2023-07-26 19:54:16 +0530

    nextJS & tailwind project inititated

---
+++ src/app/layout.js
@@ -0,0 +1,19 @@
+import './globals.css'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ subsets: ['latin'],weight:['400'] })
+
+export const metadata = {
+  title: 'Sumat Mallick Web Resume',
+  description: 'Web developer with expertise in HTML, CSS, JavaScript, React, Node.js, MongoDB, Express.js & Next.js. Building robust and responsive web pages.',
+  robots: 'index, follow'
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  )
+}
